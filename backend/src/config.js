5044291ec0eb991bf23204278fce52b09ef658b8
PythonWeb6 <pythonteam06@gmail.com>
2023-01-17 07:40:20 +0700

    updated config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -60,7 +60,7 @@ const OWNER_ADDRESS = '0x2Ae22245F644A99538Ac188A913d094b08F9E8b9';
 const TREASURY_ADDRESS = '0x2Ae22245F644A99538Ac188A913d094b08F9E8b9';
 const MAX_SUPPLY = 100; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 10; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 2; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2023-01-23T20:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
